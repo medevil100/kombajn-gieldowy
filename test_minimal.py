3bfaf29ede1c16727eeff3d2c854847774e9123e
medevil100 <szelag-adam@t-online.de>
2026-07-06 17:07:18 +0200

    Create test_minimal.py

---
+++ test_minimal.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="TEST", page_icon="🧪", layout="wide")
+st.title("🧪 TEST DZIAŁANIA STREAMLIT")
+
+st.write("✔ Streamlit działa — kod się wykonuje.")
